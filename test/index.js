2db1472d91d0835bb82973e9858ba624ffae5bd4
Prinzhorn <alex@nder-prinzhorn.de>
2014-02-06 12:38:57 +0100

    some tests

--- test/index.js
+++ test/index.js
@@ -1,3 +1,4 @@
+var assert = require('assert');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var encrypt = require('../lib');
@@ -6,7 +7,7 @@ mongoose.connect('mongodb://localhost/mongoose-encrypt');
 mongoose.set('debug', true);
 
 var userSchema = new Schema({
-	name: String,
+	_id: String,
 	secret1: String,
 	secret2: String
 });
@@ -20,23 +21,81 @@ userSchema.plugin(encrypt, {
 
 var User = mongoose.model('User', userSchema);
 
-User.remove({}, function(err) {
-	if(err) {
-		console.error(err);
-	}
-
-	var user = new User({
-		name: 'Alexander',
-		secret1: 'secret'
+describe('Basic encryption', function() {
+	before(function(done) {
+		//Clear the DB.
+		User.remove({}, done);
 	});
 
-	user.save(function(err) {
-		if(err) {
-			console.error(err);
-		}
-
-		User.findOne().exec(function(err, user) {
-			console.log(user.secret1);
+	it('should successfully save a model', function(done) {
+		var user = new User({
+			_id: 'user1',
+			secret1: 'secret'
 		});
+
+		user.save(done);
+	});
+
+	it('should return an encrypted value when bypassing the plugin by using lean()', function(done) {
+		User.findOne({_id: 'user1'}).lean().exec(function(err, user) {
+			assert.ifError(err);
+
+			assert.notEqual(user.secret1, 'secret', 'The secret1 is not in plain text.');
+			assert(/^ENCRYPTED___/.test(user.secret1), 'The secret1 starts with the magic prefix.');
+
+			assert.strictEqual(user.secret2, undefined, 'The secret2 is not set.');
+
+			done();
+		});
+	});
+
+	it('should decrypt the value when using Mongoose as usual', function(done) {
+		User.findOne({_id: 'user1'}).exec(function(err, user) {
+			assert.ifError(err);
+
+			assert.strictEqual(user.secret1, 'secret', 'The secret1 is readable.');
+			assert.strictEqual(user.secret2, undefined, 'The secret2 is not set.');
+
+			done();
+		});
+	});
+
+	it('should handle value updates by encrypting the new value', function(done) {
+		User.findOne({_id: 'user1'}).exec(function(err, user) {
+			assert.ifError(err);
+
+			user.secret1 = 'new secret';
+			user.secret2 = 'new secret 2';
+
+			user.save(function(err) {
+				assert.ifError(err);
+
+				User.findOne({_id: 'user1'}).exec(function(err, user) {
+					assert.ifError(err);
+
+					assert.strictEqual(user.secret1, 'new secret', 'The secret1 has been updated.');
+					assert.strictEqual(user.secret2, 'new secret 2', 'The secret2 has been updated.');
+
+					done();
+				});
+			});
+		});
+	});
+
+	it('should leave unset properties untouched', function(done) {
+		//Only fetch the _id property.
+		User.findOne({_id: 'user1'}, {_id: 1}).exec(function(err, user) {
+			assert.ifError(err);
+
+			assert.strictEqual(user.secret1, undefined, 'The secret1 is not set.');
+			assert.strictEqual(user.secret2, undefined, 'The secret2 is not set.');
+
+			done();
+		});
+	});
+
+	after(function(done) {
+		//Clear the DB.
+		User.remove({}, done);
 	});
 });
